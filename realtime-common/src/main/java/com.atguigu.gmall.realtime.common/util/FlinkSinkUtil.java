5458f9fb8a427857e27525e09fa9b96d8d4f5cb1
Xiaotian <2489626895@qq.com>
2024-05-03 21:02:58 +0800

    DWD 模块完成

--- realtime-common/src/main/java/com.atguigu.gmall.realtime.common/util/FlinkSinkUtil.java
+++ realtime-common/src/main/java/com.atguigu.gmall.realtime.common/util/FlinkSinkUtil.java
@@ -1,12 +1,18 @@
 package com.atguigu.gmall.realtime.common.util;
 
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall.realtime.common.bean.TableProcessDwd;
 import com.atguigu.gmall.realtime.common.constant.Constant;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.connector.sink2.Sink;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class FlinkSinkUtil {
@@ -25,4 +31,26 @@ public class FlinkSinkUtil {
                 .setProperty("transaction.timeout.ms", 15 * 60 * 1000 + "")
                 .build();
     }
+
+    public static Sink<Tuple2<JSONObject, TableProcessDwd>> getKafkaSink() {
+        return KafkaSink.<Tuple2<JSONObject, TableProcessDwd>>builder()
+                .setBootstrapServers(Constant.KAFKA_BROKERS)
+                .setRecordSerializer(new KafkaRecordSerializationSchema<Tuple2<JSONObject, TableProcessDwd>>() {
+                    @Nullable
+                    @Override
+                    public ProducerRecord<byte[], byte[]> serialize(Tuple2<JSONObject, TableProcessDwd> dataWithConfig,
+                                                                    KafkaSinkContext context,
+                                                                    Long timestamp) {
+                        String topic = dataWithConfig.f1.getSinkTable();
+                        JSONObject data = dataWithConfig.f0;
+                        data.remove("op_type");
+                        return new ProducerRecord<>(topic, data.toJSONString().getBytes(StandardCharsets.UTF_8));
+                    }
+                })
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .setTransactionalIdPrefix("atguigu-" + new Random().nextLong())
+                .setProperty("transaction.timeout.ms", 15 * 60 * 1000 + "")
+                .build();
+    }
+
 }
